5b4397d706f1db5be478a658602fde2d6f444c00
mobeets <mobeets@gmail.com>
2016-03-03 15:03:49 -0500

    loading any date

--- +io/getDates.m
+++ +io/getDates.m
@@ -1,3 +1,22 @@
-function dts = getDates()
-    dts = {'20120525', '20120601', '20131125', '20131205'};
+function dts = getDates(showRaw)
+    if nargin < 1
+        showRaw = false;
+    end
+%     dts = {'20120525', '20120601', '20131125', '20131205'};
+
+    DATADIR = getpref('factorSpace', 'data_directory');
+    if showRaw
+        fnms = dir(fullfile(DATADIR, 'Jeffy'));
+        ix = [fnms.isdir] & ~strcmp({fnms.name}, '.') & ...
+            ~strcmp({fnms.name}, '..');
+        Jeffy = {fnms(ix).name};
+        fnms = dir(fullfile(DATADIR, 'Lincoln'));
+        ix = [fnms.isdir] & ~strcmp({fnms.name}, '.') & ...
+            ~strcmp({fnms.name}, '..');
+        Lincoln = {fnms(ix).name};
+        dts = [Jeffy Lincoln];
+    else
+        fnms = dir(fullfile(DATADIR, 'preprocessed'));
+        dts = strrep({fnms(~[fnms.isdir]).name}, '.mat', '');
+    end
 end
--- +io/loadRawDataByDate.m
+++ +io/loadRawDataByDate.m
@@ -1,22 +1,40 @@
 function D = loadRawDataByDate(dtstr)
 
     DATADIR = getpref('factorSpace', 'data_directory');
-
-    if strcmp(dtstr,'20120525')
-        load([DATADIR '/Jeffy/20120525/20120525simpleData.mat']);
-        load([DATADIR '/Jeffy/20120525/kalmanInitParamsFA20120525(1).mat']);
-    elseif strcmp(dtstr, '20120601')
-        load([DATADIR '/Jeffy/20120601/20120601simpleData_combined.mat']);
-        load([DATADIR '/Jeffy/20120601/kalmanInitParamsFA20120601(2).mat']);
-    elseif strcmp(dtstr, '20131125')
-        load([DATADIR '/Lincoln/20131125/20131125simpleData.mat']);
-        load([DATADIR '/Lincoln/20131125/kalmanInitParamsFA20131125_11.mat']);
-    elseif strcmp(dtstr, '20131205')
-        load([DATADIR '/Lincoln/20131205/20131205simpleData.mat']);
-        load([DATADIR '/Lincoln/20131205/kalmanInitParamsFA20131205_11.mat']);
-    else
-        error('Date not supported yet')
+    
+    dr1 = fullfile(DATADIR, 'Jeffy', dtstr);
+    dr2 = fullfile(DATADIR, 'Lincoln', dtstr);
+    fs1 = dir(dr1);
+    fs1 = {fs1(~[fs1.isdir]).name};
+    fs2 = dir(dr2);
+    fs2 = {fs2(~[fs2.isdir]).name};
+    if numel(fs1) + numel(fs2) == 0
+        error('Date not found.');
+        return;
     end
+    if numel(fs1) > 0
+        dr = dr1; fs = fs1;
+    else
+        dr = dr2; fs = fs2;
+    end
+    load(fullfile(dr, fs{1}));
+    load(fullfile(dr, fs{2}));
+        
+%     if strcmp(dtstr,'20120525')
+%         load([DATADIR '/Jeffy/20120525/20120525simpleData.mat']);
+%         load([DATADIR '/Jeffy/20120525/kalmanInitParamsFA20120525(1).mat']);
+%     elseif strcmp(dtstr, '20120601')
+%         load([DATADIR '/Jeffy/20120601/20120601simpleData_combined.mat']);
+%         load([DATADIR '/Jeffy/20120601/kalmanInitParamsFA20120601(2).mat']);
+%     elseif strcmp(dtstr, '20131125')
+%         load([DATADIR '/Lincoln/20131125/20131125simpleData.mat']);
+%         load([DATADIR '/Lincoln/20131125/kalmanInitParamsFA20131125_11.mat']);
+%     elseif strcmp(dtstr, '20131205')
+%         load([DATADIR '/Lincoln/20131205/20131205simpleData.mat']);
+%         load([DATADIR '/Lincoln/20131205/kalmanInitParamsFA20131205_11.mat']);
+%     else
+%         error('Date not supported yet')
+%     end
 
     D.datestr = dtstr;
     D.kalmanInitParams = kalmanInitParams;
